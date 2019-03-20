a26222338c4dc2c574bf8544ef2b28225d2d36e2
Jeeyoon <acetaire@naver.com>
2019-03-20 17:14:43 +0900

    initial commit

---
+++ form.js
@@ -0,0 +1,76 @@
+var brightCount = 0;
+
+
+function unEnter(target) {
+    $(target).keydown(function(e) {
+        // alert( "Handler for .keydown() called." );
+        $(target).each(function(key, value) {
+            var that = $(this);
+            setTimeout(function(){
+                if (that.is(":focus")) {
+                    that.val(that.val().slice(0,-2));
+                } else {
+                    that.val(that.val().slice(0,-1));
+                }
+                introCopy.html(p5field.value());
+                that.width(that.width()-50);
+                that.css("opacity", that.css("opacity")-0.02);
+                if (that.val() == "") {
+                    that.hide();
+                    brightCount += 1;
+                    var bgString = "hsl("+random(280,340)+","+random(80,100)+"%,"+(20+2*brightCount)+"%)"
+                    $("html").css("background-color", bgString);
+                }
+            }, 1);
+          });
+        // e.preventDefault();
+        // e.stopPropagation();
+    })
+}
+
+function unCheck(target) {
+    $(target).click(function(e) {
+        $(target).each(function() {
+            var that = $(this);
+                if (that.find(".checkTarget").is(":checked") == false) {
+                    that.hide();
+                    brightCount += 1;
+                    var bgString = "hsl("+random(280,340)+","+random(80,100)+"%,"+(20+2*brightCount)+"%)"
+                    $("html").css("background-color", bgString);
+                }
+        });
+    });
+}
+
+function main() {
+    unEnter(".aTarget");
+    unEnter(".bTarget");
+    unEnter(".cTarget");
+    unEnter(".dTarget");
+    unCheck(".cBox");
+
+    $("form").submit(function(e){
+        var check = 1;
+        $('form .checkInput').each(function() {
+            console.log($(this).val());
+            if($(this).val()!=false) {
+                check = 0;
+            }
+        });
+        $('form .checkCbox').each(function() {
+            console.log($(this).is(":checked"));
+            if($(this).is(":checked") != false) {
+                check = 0;
+            }
+        });
+        if (check == 0) {
+            alert("필수 항목은 빈 칸으로 남겨놓아야 합니다.");
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    });
+}
+
+$(document).ready(function(){
+
+});
---
+++ formP5.js
@@ -0,0 +1,116 @@
+function windowResized() {
+    console.log('resized');
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+var p5field;
+var introCopy;
+var count = 0;
+
+let xpos, ypos; // Starting position of shape
+let xspeed = 1; // Speed of the shape
+let yspeed = 1; // Speed of the shape
+let xdirection = 1; // Left or Right
+let ydirection = 1; // Top to Bottom
+let inputWidth = 1;
+let inputHeight = 1;
+
+var 빙글빙글할까 = '시러';
+var 몇빙글 = [];
+var 첨빙글 = [];
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0,0);
+    canvas.style('z-index','-1');
+
+    // create input
+    p5field = createInput('enter text');
+    p5field.class('aTarget')
+    main();
+
+    // text that changes with input
+    introCopy = createP(p5field.value());
+    introCopy.class('intro');
+    introCopy.position(500,200);
+
+    //bg
+    var bgString = "hsl("+random(280,340)+","+random(80,100)+"%,"+20+"%)"
+    $("html").css("background-color", bgString);
+
+    //bounce
+    xpos = windowWidth / 2;
+    ypos = windowHeight / 2;
+
+
+    $('input').each(function(key, value){
+        첨빙글[key] = (random(-10, 10) * 0.03);
+        몇빙글[key] = 0;
+    });
+}
+
+function drawBackground() {
+    background($("html").css("background-color"));
+    noStroke();
+    for (i=0; i<16; i++) {
+        for (k=0; k<16; k++) {
+            if (k % 2 == 1) {
+                var xCoor = 40 + windowWidth / 16*i - count;
+            } else {
+                var xCoor = windowWidth / 16*i + count;
+            }
+            var yCoor = 40 + k * 100; 
+            if (xCoor > windowWidth) {
+                xCoor = xCoor % windowWidth;
+            } else if (xCoor < 0) {
+                xCoor = windowWidth + (xCoor % windowWidth);
+            }
+            ellipse(xCoor,yCoor,40);
+            fill(255,255,255);
+        }
+    }
+
+    뀨();
+}
+
+// function keyTyped(){
+
+// }
+// function keyReleased(){
+//     background(0,0,200);
+// }
+
+function draw() {
+    count += 1;
+    textSize(500);
+	text(count,500,500);
+    drawBackground();
+    p5field.input(updateText);
+
+    xpos = xpos + xspeed * xdirection;
+    ypos = ypos + yspeed * ydirection;
+    if (xpos > windowWidth - inputWidth || xpos < 0) {
+        xdirection *= -1;
+      }
+    if (ypos > windowHeight - inputHeight || ypos < 0) {
+        ydirection *= -1;
+      }
+    p5field.position(xpos,ypos);
+}
+
+function updateText() {
+    introCopy.html(p5field.value());
+}
+
+function 뀨() {
+
+    $('input').hover(function(){
+        빙글빙글할까 = '하자!';
+    });
+    if(빙글빙글할까 == '하자!'){
+        $('input').each(function(key, value){
+            몇빙글[key] = 몇빙글[key] + 첨빙글[key];
+            $(this).css('transform', 'rotate(' + 몇빙글[key] % 360 + 'deg)');
+        });
+    }
+}
